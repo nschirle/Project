5dd2a4361007e5cc78399db75b7347eeff0dad23
Nathan Schirle <schirle.nathan@gmail.com>
2018-10-02 19:25:48 -0700

    Add project files.

---
+++ Financial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Financial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
