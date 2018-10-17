88f0ec5c0e85bba9887e0aa7d81a640817ef457d
Nathan Schirle <schirle.nathan@gmail.com>
2018-10-16 17:29:13 -0700

    end of class tuesday

---
+++ BankApp/BankApp/BankModel.cs
@@ -0,0 +1,23 @@
+﻿
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class BankModel : DbContext
+    {
+       public BankModel() : base()
+        {
+
+        }
+        public BankModel(DbContextOptions<BankModel> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+    }
+}
