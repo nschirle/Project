185229e84c60bf1fc973dc20a59d4a9ca0b8404e
Nathan Schirle <schirle.nathan@gmail.com>
2018-11-20 14:16:19 -0800

    neww

--- FinancialWebApp/Data/Migrations/20181120221345_newinvest.Designer.cs
+++ FinancialWebApp/Data/Migrations/20181120221345_newinvest.Designer.cs
@@ -4,14 +4,16 @@ using FinancialWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinancialWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181120221345_newinvest")]
+    partial class newinvest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
